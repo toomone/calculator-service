2ab7be3f054fb85b0b2ae6fe8d0a63f9ed122d61
Tugdual Grall <tgrall@github.com>
2021-07-07 17:50:56 +0200

    Add new button

--- src/main/java/com/octodemo/rest/calculatorservice/service/CalculatorService.java
+++ src/main/java/com/octodemo/rest/calculatorservice/service/CalculatorService.java
@@ -1,12 +1,9 @@
 package com.octodemo.rest.calculatorservice.service;
 
-import com.octodemo.lib.Calculator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
-
-    Calculator calculator = new Calculator();
 
     public int addition(int a, int b){
         return a + b;
